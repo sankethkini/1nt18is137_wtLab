7c663e59f8fa4525d0253302e7ca36e5f8042338
sankethsolo <kinisanketh@gmail.com>
2021-04-01 15:00:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var a=10
+
+let greeter = "say hi "
+var times =4
+if(times >3 ){
+    let greeter="say hello"
+    
+}
+
+console.log(greeter)
+
+const greeting = {
+    message:"Abc",
+    number:2
+}
+
+greeting.message="aca"
+alert(greeter)
+
+var car;
+console.log(car+" "+typeof car)
+
+car=" "
+console.log(car+" "+typeof car)
+
+car={
+    name:"sanket",
+    usn:"1nt18is137"
+}
+
+car=null
+console.log(typeof car)
+
+function func(){
+    let a=document.getElementById("a").value
+    let b=document.getElementById("b").value
+    alert(Number(a)+Number(b))
+}
